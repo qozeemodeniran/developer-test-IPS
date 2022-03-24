c9c25b9542b2eafbe572385b870f64c5abfde349
Qozeem Odeniran <muhdqozeem@gmail.com>
2022-03-24 23:41:06 +0100

    created and registered listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,25 @@ namespace App\Providers;
 
 use App\Events\LessonWatched;
 use App\Events\CommentWritten;
+
+use App\Listeners\FirstCommentWritten;
+use App\Listeners\ThreeCommentsWritten;
+use App\Listeners\FiveCommentsWritten;
+use App\Listeners\TenCommentWritten;
+use App\Listeners\TwentyCommentWritten;
+
+use App\Listeners\FirstLessonWatched;
+use App\Listeners\FiveLessonsWatched;
+use App\Listeners\TenLessonsWatched;
+use App\Listeners\TwentyFiveLessonsWatched;
+use App\Listeners\FiftyLessonsWatched;
+
+use App\Listeners\Beginner;
+use App\Listeners\Intermediate;
+use App\Listeners\Advanced;
+use App\Listeners\Master;
+
+
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -17,9 +36,26 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         CommentWritten::class => [
             //
+            FirstCommentWritten::class,
+            ThreeCommentsWritten::class,
+            FiveCommentsWritten::class,
+            TenCommentWritten::class,
+            TwentyCommentWritten::class,
         ],
         LessonWatched::class => [
             //
+            FirstLessonWatched::class,
+            FiveLessonsWatched::class,
+            TenLessonsWatched::class,
+            TwentyFiveLessonsWatched::class,
+            FiftyLessonsWatched::class,
+        ],
+        Badges::class => [
+            //
+            Beginner::class,
+            Intermediate::class,
+            Advanced::class,
+            Master::class,
         ],
     ];
 
